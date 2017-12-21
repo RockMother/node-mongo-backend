22eddb47611ef41ef5f48e634c2bc4681ca9e2be
Kirill Shumilov <kirill.shumilov@gmail.com>
2017-12-21 16:37:38 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp'),
+    nodemon  = require("gulp-nodemon");
+
+gulp.task('default', () => {
+   nodemon({
+       script: 'index.js',
+       ext: 'js',
+       env: {
+           PORT: 8000
+       },
+       ignore: ['../node_modules/**']
+   });
+});
