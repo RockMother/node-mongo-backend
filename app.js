a73d204cd58178ca9641eb4676bf299fb7f8ec31
Kirill Shumilov <kirill.shumilov@gmail.com>
2017-12-22 15:28:27 +0800

    Added CORS allowed middleware

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ mongoose.connect(process.env.MONGODB_URI, {
     useMongoClient: true
 });
 
-app.use(require('./controllers'));
+
+app.use(require('./middlewares/cors'));
+app.use('/api', require('./controllers'));
 
 app.listen(port, () => {
     console.log(`App runned on the port ${port}`);
