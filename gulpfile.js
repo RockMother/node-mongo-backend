7ca670ac49a5b0d54a587fb01307998d02e7ab1e
Kirill Shumilov <kirill.shumilov@gmail.com>
2017-12-29 23:39:59 +0800

    Added dotenv and added .env to git ignore

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,6 @@ gulp.task('default',  () => {
        script: 'src/app.js',
        ext: 'js',
        nodeArgs: ['--inspect'],
-       env: {
-           PORT: process.env.PORT || 8000,
-           MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/node-mongo-backend-database'
-       },
        ignore: ['../node_modules/**']
    });
 });
