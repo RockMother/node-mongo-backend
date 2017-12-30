2337981d260363de673d6856b918ec8a33fc34c8
mpakunderscore <mpakunderscore@gmail.com>
2017-12-30 07:50:54 +0300

    Ups

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,3 @@ gulp.task('default',  () => {
        ignore: ['../node_modules/**']
    });
 });
-
-gulp.task('heroku', () => {
-    nodemon({
-        script: 'src/app.js',
-        ext: 'js',
-        env: {
-            PORT: 8000,
-            MONGODB_URI: 'mongodb://heroku_jn009l6b:elm0hevpg007e6opsd5d0ei2mn@ds157653.mlab.com:57653/heroku_jn009l6b'
-        },
-        ignore: ['../node_modules/**']
-    });
-});
