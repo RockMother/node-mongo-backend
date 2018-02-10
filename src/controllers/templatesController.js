b4950581675afb5b5b8c723ac53a48a32d4b1bc9
Kirill Shumilov <kirill.shumilov@gmail.com>
2018-02-10 14:15:24 +0630

    Added POST, PUT and DELETE for templates

--- src/controllers/templatesController.js
+++ src/controllers/templatesController.js
@@ -11,4 +11,47 @@ router.get('/', function (req, res) {
     });
 });
 
+function getTemplateFromBody(req) {
+    return {
+        title: req.body.title,
+        template: req.body.template
+    };
+}
+
+router.post('/', function (req, res) {
+    template.create(getTemplateFromBody(req), function (err, model) {
+        if (err)
+            return res.status(500).send(err);
+        else {
+            res.status(201).send(model);
+        }
+    })
+});
+
+router.put('/', function (req, res) {
+    template.findById(req.body._id, function (err, model) {
+        if (err)
+            res.status(500).send(err);
+        else {
+            model.set(getTemplateFromBody(req));
+            model.save(function (err, updatedModel) {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    res.status(200).send(updatedModel);
+                }
+            });
+        }
+    })
+});
+
+router.delete('/:templateId', function (req, res) {
+    template.remove({ _id: req.params.templateId }, function (err) {
+        if (err)
+            res.status(500).send(err);
+        else
+            res.sendStatus(200);
+    });
+});
+
 module.exports = router;
