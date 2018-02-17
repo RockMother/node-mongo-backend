e1bd8b5e851640bdd0db21dc7d189f3b3739b0a5
mpakunderscore <mpakunderscore@gmail.com>
2018-02-17 13:39:46 +0300

    More templates

--- testdata/templates.js
+++ testdata/templates.js
@@ -2,53 +2,70 @@ const Template = require('./../src/models/template');
 
 module.exports = () => {
     return Promise.all(
-        [{
-            title: 'Image left. Text right',
-            template: `<div class = "row">
-                        <div class="half template-image"><img src="./cloud.png"></div>
-                        <div class="half template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
-                    </div>`
-        },
+        [
             {
-                title: 'Just text',
-                template: `<div class="template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file. This harms readability and maintainability. If you find that a function’s definition is large or complex enough that it is interfering with understanding the rest of the file, then perhaps it’s time to extract it to its own module! Don’t forget to explicitly name the expression, regardless of whether or not the name is inferred from the containing variable (which is often the case in modern browsers or when using compilers such as Babel). This eliminates any assumptions made about the Error's call stack.</div>`
+                title: 'Image left. Text right',
+                template: `<div class = "row">
+                           <div class="template-image"><img src="./cloud.png"></div>
+                           <div class="template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
+                           </div>`
+            },
+            {
+                title: 'A lot of text',
+                template: `<div class = "row">
+                           <div class="template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file. This harms readability and maintainability. If you find that a function’s definition is large or complex enough that it is interfering with understanding the rest of the file, then perhaps it’s time to extract it to its own module! Don’t forget to explicitly name the expression, regardless of whether or not the name is inferred from the containing variable (which is often the case in modern browsers or when using compilers such as Babel). This eliminates any assumptions made about the Error's call stack.</div>
+                           </div>`
             },
             {
                 title: 'Image',
-                template: `<div class="template-image"></div>`
+                template: `<div class = "row">
+                           <div class="template-image"><img src="./cloud.png"></div>
+                           </div>`
             },
             {
                 title: 'Image right. Text left',
                 template: `<div class = "row">
-                        <div class="half template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
-                        <div class="half template-image"></div>
-                    </div>`
+                           <div class="template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
+                           <div class="template-image"><img src="./god.png"></div>
+                           </div>`
             },
             {
                 title: 'Image above Text',
                 template: `<div class = "column">
-                        <div class="half template-image"></div>
-                        <div class="half template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
-                    </div>`
+                           <div class="template-image"><img src="./wall.png"></div>
+                           <div class="template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
+                           </div>`
             },
             {
                 title: 'Image above Text',
                 template: `<div class = "column">
-                        <div class="half template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
-                        <div class="half template-image"></div>
-                    </div>`
+                           <div class="template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
+                           <div class="template-image"><img src="./wall.png"></div>
+                           </div>`
             },
             {
                 title: 'Image right. Text left',
-                template: `<div class = "column"><div class="half template-image"></div>
-                           <div class="half template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
-                           <div class="half template-image"></div></div>`
+                template: `<div class = "column">
+                           <div class="template-image"><img src="./cloud.png"></div>
+                           <div class="template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
+                           <div class="template-image"><img src="./god.png"></div></div>
+                           </div>`
             },
             {
                 title: 'Image right. Text left',
-                template: `<div class = "row"><div class="half template-image"></div>
-                           <div class="half template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
-                           <div class="half template-image"></div></div>`
+                template: `<div class = "row">
+                           <div class="template-image"><img src="./cloud.png"></div>
+                           <div class="template-image"><img src="./wall.png"></div>
+                           <div class="template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
+                           </div>`
+            },
+            {
+                title: 'Image right. Text left',
+                template: `<div class = "row">
+                           <div class="template-image"><img src="./cloud.png"></div>
+                           <div class="template-title">Why? Function declarations are hoisted, which means that it’s easy - too easy - to reference the function before it is defined in the file.</div>
+                           <div class="template-image"><img src="./wall.png"></div>
+                           </div>`
             },
         ].map(template => {
             return new Promise((resolve, reject) => {
