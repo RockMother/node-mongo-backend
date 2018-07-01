8c6f3345932825ebc8778670c812b6139ab7b685
Pavel Kuzmin <mpakunderscore@gmail.com>
2018-07-01 15:08:00 +0300

    fs.exists('.env.development') give me undefined

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
-let envConfig = dotenv.parse(fs.exists('.env.development')? fs.readFileSync('.env.development'): fs.readFileSync('.env'));
+console.log(fs.exists('.env.development')); //undefined :O
+let envConfig = dotenv.parse(fs.exists('.env.development') ? fs.readFileSync('.env') : fs.readFileSync('.env.development')); // ;)
 for (var k in envConfig) {
     process.env[k] = envConfig[k]
 }
